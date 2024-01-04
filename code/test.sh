22288476ce8498a6dba49380e4ec93d22191bc09
Liu Yixiang <84435436+LyxDLiI@users.noreply.github.com>
2024-01-04 13:58:50 +0800

    Update test.sh

--- code/test.sh
+++ code/test.sh
@@ -6,5 +6,5 @@ python -u test_WSL.py --img_class faz --num_classes 2 --in_chns 1 --root_path ..
 python -u test_WSL.py --img_class odoc --num_classes 3 --in_chns 3 --root_path ../data --img_class odoc --exp odoc/ --sup_type scribble --model unet
 
 # Polyp
-python -u test_WSL.py --img_class polyp --num_classes 2 --in_chns 3 --root_path ../data --img_class polyp --exp polyp/ --sup_type scribble --model unet
+python -u test_WSL.py --img_class polyp --num_classes 2 --in_chns 3 --root_path ../data --img_class polyp --exp polyp/ --sup_type block--model unet
 
